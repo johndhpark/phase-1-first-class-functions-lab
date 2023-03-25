531152b8e887fe8567d9694fcdd43a4566d1045d
John DH. Park <john.dh.park@me.com>
2023-03-24 21:26:03 -0700

    complete first class functions advanced

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+
+function returnFirstTwoDrivers(drivers) {
+	return [drivers[0], drivers[1]];
+}
+
+function returnLastTwoDrivers(drivers) {
+	return [drivers[drivers.length - 2], drivers[drivers.length - 1]];
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultipler(argument) {
+	// body...
+}
+
+function createFareMultiplier(multiplier) {
+	return (value) => {
+		return value * multiplier;
+	};
+}
+
+function fareDoubler(value) {
+	return value * 2;
+}
+
+function fareTripler(value) {
+	return value * 3;
+}
+
+function selectDifferentDrivers(arrayOfDrivers, func) {
+	return func(arrayOfDrivers);
+}
